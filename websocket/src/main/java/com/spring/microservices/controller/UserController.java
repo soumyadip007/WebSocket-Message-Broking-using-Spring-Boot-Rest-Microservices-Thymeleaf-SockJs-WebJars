d5bce6f6918222b09a3dde60d2d8f683b7bcbab3
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2020-05-25 20:21:56 +0530

    User controller scan

--- websocket/src/main/java/com/spring/microservices/controller/UserController.java
+++ websocket/src/main/java/com/spring/microservices/controller/UserController.java
@@ -3,6 +3,7 @@ package com.spring.microservices.controller;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import com.spring.microservices.model.User;
 import com.spring.microservices.model.UserResponse;
@@ -14,6 +15,11 @@ public class UserController {
     @MessageMapping("/user")
     @SendTo("/topic/user")
     public UserResponse getUser(User user) {
+    	System.out.println("TEST");
+    	System.out.println("TEST");
+    	System.out.println("TEST");
+    	System.out.println("TEST");
+    	System.out.println("TEST");
     	System.out.println(user.getName());
     	System.out.println(user.getName());
     	System.out.println(user.getName());
