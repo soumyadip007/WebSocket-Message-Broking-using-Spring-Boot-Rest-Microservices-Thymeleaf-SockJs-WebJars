3c52f06ce86a1a9efcc6340840989026397d1a79
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2020-05-25 21:02:26 +0530

    Web Socket Config

--- websocket/src/main/java/com/spring/microservices/websocket/config/WebSocketConfig.java
+++ websocket/src/main/java/com/spring/microservices/websocket/config/WebSocketConfig.java
@@ -2,6 +2,7 @@ package com.spring.microservices.websocket.config;
 
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
@@ -9,7 +10,8 @@ import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer {
-    @Override
+  
+	@Override
     public void registerStompEndpoints(StompEndpointRegistry stompEndpointRegistry) {
         stompEndpointRegistry.addEndpoint("/websocket")
                 .withSockJS();
