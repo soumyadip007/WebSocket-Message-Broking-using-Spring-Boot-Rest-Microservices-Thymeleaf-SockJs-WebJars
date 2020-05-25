749a67a31473e33adb689c239d03b5454c72b582
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2020-05-25 19:50:53 +0530

    user added

---
+++ websocket/src/main/java/com/spring/microservices/model/User.java
@@ -0,0 +1,16 @@
+package com.spring.microservices.model;
+
+public class User {
+
+	private String user;
+
+	public String getUser() {
+		return user;
+	}
+
+	public void setUser(String user) {
+		this.user = user;
+	}
+	
+	
+}
