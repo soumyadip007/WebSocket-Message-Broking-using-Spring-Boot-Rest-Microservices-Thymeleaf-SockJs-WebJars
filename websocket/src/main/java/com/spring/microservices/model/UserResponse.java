d1b9fa3c1b4bceda498a6cc1c89095a077df7b86
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2020-05-25 19:51:53 +0530

    User response

---
+++ websocket/src/main/java/com/spring/microservices/model/UserResponse.java
@@ -0,0 +1,19 @@
+package com.spring.microservices.model;
+
+public class UserResponse {
+
+	private String content;
+
+	public UserResponse() {
+	}
+
+	public String getContent() {
+		return content;
+	}
+
+	public void setContent(String content) {
+		this.content = content;
+	}
+	
+	
+}
