a8f8196ec86c448faaf023dc2723de115a7b8aeb
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2020-05-25 19:45:27 +0530

    Method Implemented

--- websocket/src/main/java/com/spring/microservices/websocket/config/WebSocketConfig.java
+++ websocket/src/main/java/com/spring/microservices/websocket/config/WebSocketConfig.java
@@ -7,7 +7,7 @@ public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer  {
 
 	@Override
 	public void registerStompEndpoints(StompEndpointRegistry registry) {
-		
+		registry.addEndpoint("/websocket").withSockJS();
 	}
 
 }
