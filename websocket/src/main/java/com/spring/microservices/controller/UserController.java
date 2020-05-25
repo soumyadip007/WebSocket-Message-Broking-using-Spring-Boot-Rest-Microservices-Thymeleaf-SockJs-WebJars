00bbca7864b5afdea9e44c3e7e8c7d7ed430f5a3
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2020-05-25 19:55:37 +0530

    Controller added

--- websocket/src/main/java/com/spring/microservices/controller/UserController.java
+++ websocket/src/main/java/com/spring/microservices/controller/UserController.java
@@ -1,6 +1,8 @@
 package com.spring.microservices.controller;
 
 import org.springframework.boot.autoconfigure.security.SecurityProperties.User;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 
 import com.spring.microservices.model.UserResponse;
@@ -8,6 +10,8 @@ import com.spring.microservices.model.UserResponse;
 @Controller
 public class UserController {
 
+	@MessageMapping("/user")
+	@SendTo("/topic/user")
 	public UserResponse getUser(User user) {
 		
 		return new UserResponse(user.getName());
