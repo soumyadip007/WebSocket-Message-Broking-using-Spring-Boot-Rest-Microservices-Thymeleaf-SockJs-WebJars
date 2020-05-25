0d045ce7846d679f3bdf5915e01771b09c26e435
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2020-05-25 20:16:00 +0530

    user controller

--- websocket/src/main/java/com/spring/microservices/controller/UserController.java
+++ websocket/src/main/java/com/spring/microservices/controller/UserController.java
@@ -1,19 +1,26 @@
 package com.spring.microservices.controller;
 
-import org.springframework.boot.autoconfigure.security.SecurityProperties.User;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
 
+import com.spring.microservices.model.User;
 import com.spring.microservices.model.UserResponse;
 
 @Controller
 public class UserController {
 
-	@MessageMapping("/user")
-	@SendTo("/topic/user")
-	public UserResponse getUser(User user) {
-		
-		return new UserResponse(user.getName());
-	}
+
+    @MessageMapping("/user")
+    @SendTo("/topic/user")
+    public UserResponse getUser(User user) {
+    	System.out.println(user.getName());
+    	System.out.println(user.getName());
+    	System.out.println(user.getName());
+    	System.out.println(user.getName());
+    	System.out.println(user.getName());
+    	System.out.println(user.getName());
+    	System.out.println(user.getName());
+        return new UserResponse("Hi " + user.getName());
+    }
 }
