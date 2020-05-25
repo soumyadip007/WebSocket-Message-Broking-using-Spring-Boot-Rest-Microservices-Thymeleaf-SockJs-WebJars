6600467504ae9f2c84d05f190a6fa15134b70654
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2020-05-25 20:15:49 +0530

    scheduler

--- websocket/src/main/java/com/spring/microservices/websocket/config/SchedulerConfig.java
+++ websocket/src/main/java/com/spring/microservices/websocket/config/SchedulerConfig.java
@@ -6,17 +6,17 @@ import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 
+import com.spring.microservices.model.UserResponse;
+
 @EnableScheduling
 @Configuration
 public class SchedulerConfig {
 
-	@Autowired
-	SimpMessagingTemplate template;
-	
-	@Scheduled(fixedDelay = 2000)
-	public void sendMessage() {
-		template.convertAndSend("/topic/user","From auto scheduler");
-		
-	}
-	
+    @Autowired
+    SimpMessagingTemplate template;
+
+    @Scheduled(fixedDelay = 3000)
+    public void sendAdhocMessages() {
+        template.convertAndSend("/topic/user", new UserResponse("Fixed Delay Scheduler"));
+    }
 }
