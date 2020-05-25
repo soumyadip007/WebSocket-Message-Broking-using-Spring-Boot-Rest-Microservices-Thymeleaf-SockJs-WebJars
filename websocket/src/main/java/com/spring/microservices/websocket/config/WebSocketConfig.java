045cc75aea45f19e62686ace17b7b9a9ac05add8
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2020-05-25 19:47:59 +0530

    @EnableWebSocketMessageBroker

--- websocket/src/main/java/com/spring/microservices/websocket/config/WebSocketConfig.java
+++ websocket/src/main/java/com/spring/microservices/websocket/config/WebSocketConfig.java
@@ -1,9 +1,14 @@
 package com.spring.microservices.websocket.config;
 
+import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
 import org.springframework.web.socket.config.annotation.AbstractWebSocketMessageBrokerConfigurer;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 
+
+@Configuration
+@EnableWebSocketMessageBroker
 public class WebSocketConfig extends AbstractWebSocketMessageBrokerConfigurer  {
 
 	@Override
