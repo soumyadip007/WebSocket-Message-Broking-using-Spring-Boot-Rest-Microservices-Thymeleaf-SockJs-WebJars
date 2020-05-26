01e1cb0f726dbac3e299bec5bd6a886f43cd86d4
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2020-05-26 11:41:54 +0530

    Schedule cngd

--- websocket/src/main/java/com/spring/microservices/websocket/config/SchedulerConfig.java
+++ websocket/src/main/java/com/spring/microservices/websocket/config/SchedulerConfig.java
@@ -18,6 +18,6 @@ public class SchedulerConfig {
     @Scheduled(fixedDelay = 3000)
     public void sendAdhocMessages() {
         template.convertAndSend("/topic/user", new UserResponse("Fixed Delay Scheduler"));
-        System.out.println("Hello");
+        System.out.println("Hola! I'm a websocket");
     }
 }
