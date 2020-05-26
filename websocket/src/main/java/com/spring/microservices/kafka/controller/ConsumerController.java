18c0d4ce2933bfc296ac33a5c3c1c0b775fa81bc
Soumyadip Chowdhury <soumyadip.note@gmail.com>
2020-05-26 11:45:25 +0530

    Kafka with Webscoket connector

--- websocket/src/main/java/com/spring/microservices/kafka/controller/ConsumerController.java
+++ websocket/src/main/java/com/spring/microservices/kafka/controller/ConsumerController.java
@@ -1,13 +1,23 @@
 package com.spring.microservices.kafka.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
+
+import com.spring.microservices.model.UserResponse;
 
 @Service
 public class ConsumerController {
 
+	 @Autowired
+	 SimpMessagingTemplate template;
+
 	@KafkaListener(topics="Kafka_String", group = "group_id")
 	public void ConsumeString(String msg) {
+		
+		template.convertAndSend("/topic/user", new UserResponse(msg));
+	      
 		System.out.println(msg);
 	}
 
